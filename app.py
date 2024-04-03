2c22212a740dedf9e1945ff02814468959fbaa6c
Adetunji Fatai <sincerityisgood@gmail.com>
2024-04-03 12:10:35 +0100

    app.py update

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expense_tracker.db'
-db = SQLAlchemy(app)
 
-@app.route('/about', methods=['GET'])
-def about():
-    data = {
-        "name": "Adetunji Fatai B",
-        "gender": "Male",
-        "github_url": "https://github.com/Ingenious464",
-        "framework": "Flask",
-        "location": "Nigeria"
-    }
-    return jsonify(data)
+expenses = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', expenses=expenses)
+
+@app.route('/add_expense', methods=['POST'])
+def add_expense():
+    expense = request.form.get('expense')
+    expenses.append(expense)
+    return redirect('/')
+
+@app.route('/delete_expense/<int:index>', methods=['POST'])
+def delete_expense(index):
+    del expenses[index]
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
